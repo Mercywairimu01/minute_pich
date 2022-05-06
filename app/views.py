35671520a3db102ce8a7e127885d7e1342f17a8a
Mercywairimu01 <mercy.mambui@student.moringaschool.com>
2022-05-06 21:18:20 +0300

    add a base template for extensions

--- app/views.py
+++ app/views.py
@@ -1,4 +1,3 @@
-from email import message
 from flask import render_template
 from app import app
 
@@ -9,5 +8,6 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
+    title = 'MINUTE PITCH'
     message ='welcome to minute pitches'
-    return render_template('index.html',message =message)
+    return render_template('index.html',message =message,title= title)
