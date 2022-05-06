89d5c1e0823676b09d2aec2354a7d4c7552681b9
Mercywairimu01 <mercy.mambui@student.moringaschool.com>
2022-05-06 15:15:55 +0300

    add a run.py and test if application is running

--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+from email import message
 from flask import render_template
 from app import app
 
@@ -8,4 +9,5 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    return render_template('index.html')
+    message ='welcome to minute pitches'
+    return render_template('index.html',message =message)
