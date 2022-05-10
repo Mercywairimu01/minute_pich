1aceed8288f10a0f498f00ae936ff32c38a27530
Mercywairimu01 <mercy.mambui@student.moringaschool.com>
2022-05-10 13:58:02 +0300

    add all exports

---
+++ start.sh
@@ -0,0 +1,7 @@
+export SECRET_KEY='12345678'
+export DATABASE_URL='postgresql+psycopg2://moringa:188023@localhost:5432/pitches'
+
+export MAIL_USERNAME='mercypitch@gmail.com'
+export MAIL_PASSWORD='pre188023'
+
+python3 manage.py server
