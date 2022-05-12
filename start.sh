487860490d41a3c8bc655452692f8138c61ea426
Mercywairimu01 <mercy.mambui@student.moringaschool.com>
2022-05-12 23:26:48 +0300

    fix os bug

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export SECRET_KEY='12345678'
-export DATABASE_URL='postgresql+psycopg2://moringa:188023@localhost:5432/pitchie'
+export DATABASE_URL='postgresql+psycopg2://moringa:188023@localhost:5433/pitchie'
 
 export MAIL_USERNAME='mercypitch@gmail.com'
 export MAIL_PASSWORD='pre188023'
