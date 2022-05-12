88448540854faed89ecba28333700dc692ed98bd
Mercywairimu01 <mercy.mambui@student.moringaschool.com>
2022-05-12 16:16:23 +0300

    fix the os bug

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     '''
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     SECRET_KEY =os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:188023@localhost/pitchie'
     SQLALCHEMY_TRACK_MODIFICATIONS=True
     
     #  email configurations
