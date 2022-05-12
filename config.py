2b004651ca36c40a1b34f3106a07943b9c656cef
Mercywairimu01 <mercy.mambui@student.moringaschool.com>
2022-05-12 17:49:18 +0300

    fix the os bug

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ class Config:
     '''
     UPLOADED_PHOTOS_DEST ='app/static/photos'
     SECRET_KEY =os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:188023@localhost/pitchie'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS=True
 
     #  email configurations
@@ -22,11 +22,10 @@ class ProdConfig(Config):
     Args:
         Config: The parent configuration class with General configuration settings
     '''
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://moringa:188023@localhost/pitchie'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL').replace("://", "ql://",1)
 
-    if SQLALCHEMY_DATABASE_URI.startswith('postgres://'):
-        SQLALCHEMY_DATABASE_URI = SQLALCHEMY_DATABASE_URI.replace('postgres://', "postgresql://", 1)
    
+    DEBUG = True
    
 class DevConfig(Config):
     '''
@@ -35,7 +34,7 @@ class DevConfig(Config):
         Config: The parent configuration class with General configuration settings
     '''
    
-    DEBUG = True
+ 
   
 config_options = {
 'development':DevConfig,
