e5f6264004a431df631ab7b639512ec8006b8603
Mercywairimu01 <mercy.mambui@student.moringaschool.com>
2022-05-13 08:28:12 +0300

    change database url

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export SECRET_KEY='12345678'
-export DATABASE_URL='postgresql+psycopg2://moringa:188023@localhost/pitchie'
+export DATABASE_URL='postgres://clqnlqfxjsburp:8c09209caac284317663c8f513246f32db387d1e6b84ca040f078c3eb7a96c6c@ec2-54-172-175-251.compute-1.amazonaws.com:5432/d1ns9u4o75k8en'
 
 export MAIL_USERNAME='mercypitch@gmail.com'
 export MAIL_PASSWORD='pre188023'
