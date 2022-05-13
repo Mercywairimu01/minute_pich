7b7ce0d0e0d45b67f05fdc7b7640a5a550480b25
Mercywairimu01 <mercy.mambui@student.moringaschool.com>
2022-05-13 08:38:05 +0300

    fix os bug

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 export SECRET_KEY='12345678'
-export DATABASE_URL='postgres://clqnlqfxjsburp:8c09209caac284317663c8f513246f32db387d1e6b84ca040f078c3eb7a96c6c@ec2-54-172-175-251.compute-1.amazonaws.com:5432/d1ns9u4o75k8en'
+export DATABASE_URL='postgres://vgbsaupzaardno:83f966538275c67b5f050d67ace70024d2ba440081d9eb65523dd26337a9a94e@ec2-52-200-215-149.compute-1.amazonaws.com:5432/d6pr1cgrptbl47'
 
 export MAIL_USERNAME='mercypitch@gmail.com'
 export MAIL_PASSWORD='pre188023'
